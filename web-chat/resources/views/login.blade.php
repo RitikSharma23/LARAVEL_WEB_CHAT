6ba1ea4a6043c9ab877bf1b1402c45d40b888dc8
Shanu Pandey <100440136+Shanu-Kumar-Pandey@users.noreply.github.com>
2022-11-23 18:32:47 +0530

    COMMIT

--- web-chat/resources/views/login.blade.php
+++ web-chat/resources/views/login.blade.php
@@ -1,14 +1,72 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>LOGIN</title>
-</head>
-<body>
 
-<h1>Login</h1>
-    
-</body>
+<head>
+		<meta charset="utf-8">
+		<title>LOGIN</title>
+		<meta name="description" content="#">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<!-- Bootstrap core CSS -->
+		<link href="dist/css/lib/bootstrap.min.css" type="text/css" rel="stylesheet">
+		<!-- Swipe core CSS -->
+		<link href="dist/css/swipe.min.css" type="text/css" rel="stylesheet">
+		<!-- Favicon -->
+		<link href="dist/img/favicon.png" type="image/png" rel="icon">
+	</head>
+	<body class="start">
+		<main>
+			<div class="layout">
+				<!-- Start of Sign In -->
+				<div class="main order-md-1">
+					<div class="start">
+						<div class="container">
+							<div class="col-md-12">
+								<div class="content">
+									<h1>Login</h1>
+									
+									<p>Use Your Phone Number To Login </p>
+									<form>
+										<div class="form-group">
+											<input type="number" name="phone" class="form-control" placeholder="Phone" required>
+											<button class="btn icon"><i class="material-icons">local_phone</i></button>
+										</div>
+										<div class="form-group">
+											<input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+											<button class="btn icon"><i class="material-icons">lock_outline</i></button>
+										</div>
+										<button type="submit" class="btn button" formaction="index-2.html">Login</button>
+										<div class="callout">
+											<span>Don't have account? <a href="sign-up.html">Create Account</a></span>
+										</div>
+									</form>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<!-- End of Sign In -->
+				<!-- Start of Sidebar -->
+				<div class="aside order-md-2">
+					<div class="container">
+						<div class="col-md-12">
+							<div class="preference">
+								<h2>Hello, Friend!</h2>
+								<p>Enter your personal details and start your journey with Us today.</p>
+								<a href="register.blade.php" class="btn button">Register</a>
+							</div>
+						</div>
+					</div>
+				</div>
+				<!-- End of Sidebar -->
+			</div> <!-- Layout -->
+		</main>
+		<!-- Bootstrap core JavaScript
+		================================================== -->
+		<!-- Placed at the end of the document so the pages load faster -->
+		<script src="dist/js/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+		<script>window.jQuery || document.write('<script src="dist/js/vendor/jquery-slim.min.js"><\/script>')</script>
+		<script src="dist/js/vendor/popper.min.js"></script>
+		<script src="dist/js/bootstrap.min.js"></script>
+	</body>
+
 </html>
