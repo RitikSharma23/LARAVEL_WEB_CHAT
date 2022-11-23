6ca26171c2617701204728a02cdce5eb5add6dde
RitikSharma23 <100440065+RitikSharma23@users.noreply.github.com>
2022-11-23 19:14:30 +0530

    commit

--- web-chat/app/Http/Controllers/DataController.php
+++ web-chat/app/Http/Controllers/DataController.php
@@ -2,17 +2,47 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Http\Request;
 
 class DataController extends Controller
 {
     public function register(Request $request){
-        echo "<pre>";
-        print_r($request->toArray());
+        // $request->validate([
+        //     'fname'=>'required|alpha',
+        //     'lname'=>'required|alpha',
+        //     'email'=>'required|email',
+        //     'phone'=>'required|numeric|max_digits:10|min_digits:10',
+        //     'password'=>'required:',
+        //     'cnf_password'=>'required|same:password'
+        // ]);
+
+
+        $c=new User;
+        $c->fname=$request['fname'];
+        $c->lname=$request['lname'];
+        $c->email=$request['email'];
+        $c->phone=$request['phone'];
+        $c->password=Hash::make($request['password']);
+        $c->save();
+
+          if(Auth::attempt($request->only('email','password'))){
+            return redirect('/loginpage');
+          }else{
+            echo "not";
+          }
 
     }
     public function login(Request $request){
         echo "<pre>";
         print_r($request->toArray());
+
+        if(Auth::attempt($request->only('phone','password'))){
+            // return redirect('/loginpage');/
+          }else{
+            echo "not";
+          }
     }
 }
