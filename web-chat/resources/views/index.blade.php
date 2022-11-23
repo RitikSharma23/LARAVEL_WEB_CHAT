73de8742adf6b9020d263c8791f7b58ced3161d8
Shanu Pandey <100440136+Shanu-Kumar-Pandey@users.noreply.github.com>
2022-11-23 19:55:45 +0530

    commit

--- web-chat/resources/views/index.blade.php
+++ web-chat/resources/views/index.blade.php
@@ -3,7 +3,7 @@
 
 <head>
 		<meta charset="utf-8">
-		<title>Swipe – The Simplest Chat Platform</title>
+		<title>Index</title>
 		<meta name="description" content="#">
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 		<!-- Bootstrap core CSS -->
