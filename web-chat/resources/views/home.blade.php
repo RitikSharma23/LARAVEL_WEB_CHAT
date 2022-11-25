8ad77801510afa1eb6c381802feddb51d1240c0e
Vrutik11 <100480211+Vrutik11@users.noreply.github.com>
2022-11-25 21:30:38 +0530

    commit

--- web-chat/resources/views/home.blade.php
+++ web-chat/resources/views/home.blade.php
@@ -11,39 +11,83 @@
 <link rel="stylesheet" href="css.css">
 <title>HOME</title>
 </head>
-<body style="overflow:hidden;">
+<body style="overflow:hidden;background-color:#8a4ef9">
 <div class="container">
-    <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom" style="width:99vw;margin-left:-100px;">
+    <header class="d-flex justify-content-center py-3" style="width:99vw;margin-left:-120px;">
       <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
         <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"></use></svg>
-        <span class="fs-4">HOME</span>
+        <span class="fs-4" style="color:White">WEB CHAT</span>
       </a>
 
       <ul class="nav nav-pills">
         <!-- <li class="nav-item"><a href="{{url('/')}}/home" class="nav-link active" aria-current="page">Home</a></li> -->
-        <li class="nav-item" style="margin-right:170px"><a href="loginpage" class="nav-link"><button class="btn btn-info" style="color:white;width:100px;border-radius:50px;font-family: 'Montserrat', sans-serif;">Login</button></a></li>
-        <!-- <li class="nav-item"><a href="registerpage" class="nav-link">Register</a></li> -->
+        <li class="nav-item" style="margin-right:1px"><a href="loginpage" class="nav-link">
+          <button class="new">Login</button></a></li>
+        <li class="nav-item" style="margin-right:100px"><a href="registerpage" class="nav-link">
+          <button class="new">About</button></a></li>
       </ul>
     </header>
-    <img style="width:92vw;position:relative;top: -24px;" src="web.jpg">
+    <!-- <img style="width:92vw;position:relative;top: -24px;" src="web.jpg"> -->
+    <div class="bb">
+        <h1 class="hy">Have your<br><span class="ch">best chat</span></h1>
+    </div>
+    <div class="cc">
+      <p>Fast easy & Unlimited chat Experience<br>you'll ever head</p>
+    </div>
   </div> 
   <a href="registerpage"> <button class="shan">Get Started</button></a> 
+  <img class="ss" src="home.png">
 
 
   <style>
-    @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@200;300;400;500;700&display=swap'); 
-    .btn-info:hover{
-      background-color:#19647e;
+    .ss{
+      position:absolute;
+      right:300px;
+      width:400px;
+      top:150px;
+    }
+    .cc{
+      position:absolute;
+      top:370px;
+      left:205px;
+      color:white
+    }
+    .bb{
+      position:absolute;
+      top:200px;
+      left:205px;
+    }
+    .hy{
+      color:white;
+      font-size:60px;
+    }
+    .ch{
+      font-size:70px;
+      color:#f78fdb;
+    }
+    .new{
+      border-radius:100px;
+      background-color:#8a4ef9;
+      border:none;
+      color:white;
+      font-size:25px;
+      font-family:bold;
+      color:white;
+      width:120px;
+      border:1px solid white;
+    }
+    .new:hover{
+      background-color:#674a8d;
       box-shadow:0px 0px 10px 1px #59c7e3;
     }
     .shan{
       position:absolute;
-      top:450px;
+      top: 430px;px;
       left:195px;
       width:200px;
       height:60px;
       border-radius:100px;
-      background-color:#39b9ba;
+      background-color:#694c8c;
       border:none;
       color:white;
       font-size:25px;
@@ -51,8 +95,9 @@
 
     }
     .shan:hover{
-      background-color:#19647e;
+      background-color:white;
       box-shadow:0px 0px 10px 1px #59c7e3;
+      color:#694c8c
     }
   </style>
 </body>
