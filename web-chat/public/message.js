1c5bed9294ac895d5ad10a8ac46516b0d7f6be8f
RitikSharma23 <100440065+RitikSharma23@users.noreply.github.com>
2022-11-26 09:05:36 +0530

    commit

--- web-chat/public/message.js
+++ web-chat/public/message.js
@@ -172,7 +172,6 @@ function dischat(me,user){
 
     var myFunction = function() {
         var attribute = this.getAttribute("id");
-    //     // document.getElementById("mes").innerHTML=""
         dischat(me,attribute)
         user=attribute
     };
